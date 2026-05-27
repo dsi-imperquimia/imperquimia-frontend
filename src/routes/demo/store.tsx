33254f12e9e6564dcaf51351021237609c06446d
Leonel Henriquez <leonelhenriquez@users.noreply.github.com>
2026-05-26 22:29:56 -0600

    cambiar useStore a useSelector en DemoStore para mejorar la selección de estado

--- src/routes/demo/store.tsx
+++ src/routes/demo/store.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { useStore } from '@tanstack/react-store'
+import { useSelector } from '@tanstack/react-store'
 
 import { fullName, store } from '#/lib/demo-store'
 
@@ -8,7 +8,7 @@ export const Route = createFileRoute('/demo/store')({
 })
 
 function FirstName() {
-  const firstName = useStore(store, (state) => state.firstName)
+  const firstName = useSelector(store, (state) => state.firstName)
   return (
     <input
       type="text"
@@ -22,7 +22,7 @@ function FirstName() {
 }
 
 function LastName() {
-  const lastName = useStore(store, (state) => state.lastName)
+  const lastName = useSelector(store, (state) => state.lastName)
   return (
     <input
       type="text"
@@ -36,7 +36,7 @@ function LastName() {
 }
 
 function FullName() {
-  const fName = useStore(fullName, (state) => state)
+  const fName = useSelector(fullName, (state) => state)
   return (
     <div className="bg-white/10 rounded-lg px-4 py-2 outline-none ">
       {fName}
