24292cd696b9bdcf5999bf2a7b6a60917b87627b
Leonel Henriquez <leonelhenriquez@users.noreply.github.com>
2026-05-29 16:36:13 -0600

    reorganizar importaciones en vite.config.ts para mejorar la legibilidad

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
+import { defineConfig } from 'vite'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
-import viteReact, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
+import viteReact, { reactCompilerPreset } from '@vitejs/plugin-react'
 import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
@@ -18,6 +18,9 @@ const config = defineConfig({
     viteReact(),
     babel({ presets: [reactCompilerPreset()] }),
   ],
+  server: {
+    allowedHosts: ['imperquimia.local'],
+  },
 })
 
 export default config
