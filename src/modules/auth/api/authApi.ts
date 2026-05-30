055bf6902879c0427e7451435c97b281fc9c1705
Leonel Henriquez <leonelhenriquez@users.noreply.github.com>
2026-05-29 22:03:44 -0600

    agregar función de inicio de sesión con manejo de errores en authApi

---
+++ src/modules/auth/api/authApi.ts
@@ -0,0 +1,27 @@
+import { http } from "@lib/http";
+import type { User } from "@modules/auth/types";
+import axios from "axios";
+
+interface LoginResponse {
+  user: User;
+  access_token: string;
+}
+
+export async function loginRequest(
+  email: string,
+  password: string,
+): Promise<LoginResponse> {
+  try {
+    const { data } = await http.post<LoginResponse>("/auth/login", {
+      email,
+      password,
+    });
+    return data;
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      const message = (err.response?.data as { message?: string })?.message;
+      throw new Error(message ?? "Credenciales inválidas");
+    }
+    throw err;
+  }
+}
