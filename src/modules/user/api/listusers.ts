7dfe51050683eaf82bef6f99dd084bd94f4dc2a3
Leonel Henriquez <leonelhenriquez@users.noreply.github.com>
2026-06-07 12:37:03 -0600

    corregir tipo de respuesta en la función listUsers para manejar un array de usuarios

--- src/modules/user/api/listusers.ts
+++ src/modules/user/api/listusers.ts
@@ -2,6 +2,6 @@ import { http } from "@lib/http";
 import type { User } from "@modules/user/types/user";
 
 export async function listUsers() {
-  const { data } = await http.get<User>("/users");
+  const { data } = await http.get<User[]>("/users");
   return data;
 }
